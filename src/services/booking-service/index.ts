d50b887ec99a821a20f596c96280c03f92f75cb3
Hugo Gomes <gomes.hugo00@gmail.com>
2022-12-02 09:45:13 -0300

    feat: booking service

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,1 +1,49 @@
 
+import { notFoundError, unauthorizedError } from "@/errors";
+import bookingRepository from "@/repositories/booking-repository";
+import enrollmentRepository from "@/repositories/enrollment-repository";
+import ticketRepository from "@/repositories/ticket-repository";
+
+async function getBooking(userId: number) {
+  const booking = await bookingRepository.findBooking(userId);
+  if(!booking) throw notFoundError;
+
+  return booking;
+}
+
+async function insertBooking(userId: number, roomId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) {
+    throw notFoundError();
+  }
+
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+
+  if (!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
+    throw notFoundError();
+  }
+
+  const booking = await bookingRepository.createBooking(userId, roomId);
+  return { roomId: booking.id };
+}
+
+async function updateBooking(bookingId: number, roomId: number) {
+//   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+//   if (!enrollment) {
+//     throw notFoundError();
+//   }
+  
+  //   const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+  
+  //   if (!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
+  //     throw notFoundError();
+  //   }
+
+  await bookingRepository.upsertBooking(bookingId, roomId);
+}
+
+export const serviceBooking = {
+  getBooking,
+  insertBooking,
+  updateBooking
+};
